a570d3dc05c5e0a298c64254c7be736f69ff8831
mtj0928 <mtj0928@users.noreply.github.com>
2022-02-08 21:47:52 +0900

    rename

--- Projects/Apps/Home/Mock/SceneDelegate.swift
+++ Projects/Apps/Home/Mock/SceneDelegate.swift
@@ -9,7 +9,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = environment.viewResolver.resolve(for: .login)
+        window.rootViewController = environment.viewResolver.resolve(for: .home)
         window.makeKeyAndVisible()
 
         self.window = window
